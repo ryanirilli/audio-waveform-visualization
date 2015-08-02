f88f64f073aa280950ba02f4529b8d3ba4b6c41a
ryanirilli <Enter1Enter1>
2015-08-02 11:42:30 -0700

    add new homepage

---
+++ app/components/full-page.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  onDidInsertElement: function(){
+    this.setHeight();
+    this.setCoverPhoto();
+    $(window).resize(()=>{
+      Ember.run.debounce(this, this.setHeight, 100);
+    });
+  }.on('didInsertElement'),
+
+  setHeight: function(){
+    let windowHeight = $(window).outerHeight();
+    this.$().height(windowHeight);
+  },
+
+  setCoverPhoto: function(){
+    let coverPhotoPath = this.get('coverPhotoPath');
+    if(!this.get('coverPhotoPath')) { return; }
+    this.$().css({
+      background: `url(${coverPhotoPath})`,
+      'background-size': 'cover'
+    });
+  }
+});
